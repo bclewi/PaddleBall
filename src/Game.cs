535cce47e75cfbae61e1b22af754f7c642e4f257
Brandon Lewis <31356859+bclewi@users.noreply.github.com>
2024-11-08 23:42:38 -0500

    customize editorconfig and apply C# formatting

--- src/Game.cs
+++ src/Game.cs
@@ -2,12 +2,13 @@ namespace PaddleBall;
 
 using Godot;
 
-public partial class Game : Control {
-  public Button TestButton { get; private set; } = default!;
-  public int ButtonPresses { get; private set; }
+public partial class Game : Control
+{
+    public Button TestButton { get; private set; } = default!;
+    public int ButtonPresses { get; private set; }
 
-  public override void _Ready()
-    => TestButton = GetNode<Button>("%TestButton");
+    public override void _Ready()
+      => TestButton = GetNode<Button>("%TestButton");
 
-  public void OnTestButtonPressed() => ButtonPresses++;
+    public void OnTestButtonPressed() => ButtonPresses++;
 }
